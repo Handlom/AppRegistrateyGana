94b291ab62bab6699f1570dd6eb9d8e0563633ed
RogerCG <roger.canchanya.syp@handlom.com>
2016-10-05 17:50:45 -0500

    Segundo punto/checkpoint - Materializeccs + Formato de correo+formulario de registro basico

--- index.php
+++ index.php
@@ -3,19 +3,20 @@
 $correo= $_POST["correo"];
 $subject = "TEST APP HANDLOM";
 // Get HTML contents from file
-$htmlContent = file_get_contents("email_template.html");
+$htmlContent = file_get_contents("templates/email_template.html");
 
 // Set content-type for sending HTML email
 $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
 // Additional headers
-$headers .= 'From: Handlom01<roger.canchanya.syp@handlom.com>' . "\r\n";
+$headers .= 'From: Handlom - Expotextil<roger.canchanya.syp@handlom.com>' . "\r\n";
 $headers .= 'Cc:roger.canchanya.syp@handlom.com' . "\r\n";
 
 // Send email
 if(mail($correo,$subject,$htmlContent,$headers)):
-	$successMsg = 'Email has sent successfully.';
+	$successMsg = 'El email fue enviado con éxito.';
+	header("Location:form.html");
 else:
 	$errorMsg = 'Some problem occurred, please try again.';
 endif;
